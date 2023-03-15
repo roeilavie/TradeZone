fe5facac7e40a32a149bfbb64473872f5b67e4c9
moshikobitton <m6m66b@gmail.com>
2023-03-15 17:27:36 +0200

    update

--- Client side/src/index.js
+++ Client side/src/index.js
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
       <GoogleOAuthProvider clientId="427466753640-kv1aqbf0085u0i2edrf83l1mbl7ck5if.apps.googleusercontent.com">
         <App />
       </GoogleOAuthProvider>
-    </BrowserRouter>,
+    </HashRouter>,
   rootElement
 );
--- Client side/src/service/service.jsx
+++ Client side/src/service/service.jsx
@@ -1,7 +1,6 @@
 // api
 // http://localhost:49218/api
-export const api_production =
-  "https://proj.ruppin.ac.il/cgroup1/test2/tar2/api";
+export const api_production = "/cgroup1/test2/tar2/api";
 export const api_python = "https://moshiko.pythonanywhere.com/newman";
 export const validateEmail = (email) => {
   // regular expression to match email format
