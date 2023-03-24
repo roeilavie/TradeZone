cb8c343ebc1bf1100267469748f524b7cb6b8c80
roeilavie <102984425+roeilavie@users.noreply.github.com>
2023-03-24 15:57:40 +0300

    vdvd

--- Client side/src/service/service.jsx
+++ Client side/src/service/service.jsx
@@ -1,6 +1,5 @@
 // api
-// api for production: /cgroup1/test2/tar2/api
-export const api_production = "http://localhost:49218/api";
+export const api_production = "/cgroup1/test2/tar2/api/api";
 export const api_python = "https://moshiko.pythonanywhere.com/newman";
 export const validateEmail = (email) => {
   // regular expression to match email format
